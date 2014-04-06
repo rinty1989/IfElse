6fd84d2d93779fcc05ea5efea60e263c498b2b9e
rinty1989 <rinty1989@gmail.com>
2014-04-06 00:37:27 -0400

    if else hw

---
+++ IfElse/src/com/ifelse/IfHWElse.java
@@ -0,0 +1,23 @@
+package com.ifelse;
+
+public class IfHWElse {
+public static void main(String [] args){
+	int a =100;
+	if (a>=90){
+	System.out.println("A");
+}
+else if (a<90 &&  a>=80){
+	System.out.println("B");
+}
+else if ( a<79 && a>=70){
+	System.out.println("C");
+}
+else if (a <69 && a>=60){
+	System.out.println("D");
+}
+	
+else {
+	System.out.println("F");
+}
+}
+}
